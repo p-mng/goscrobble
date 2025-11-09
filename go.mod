fa2315adfb58fdc851620702a23f337ebc4cf905
Patrick Mang <65464529+p-mng@users.noreply.github.com>
2025-11-09 22:24:55 +0100

    Add tests

--- go.mod
+++ go.mod
@@ -4,14 +4,19 @@ go 1.25.3
 
 require (
 	github.com/godbus/dbus/v5 v5.1.0
+	github.com/jinzhu/copier v0.4.0
 	github.com/p-mng/lastfm-go v1.0.0
 	github.com/pelletier/go-toml/v2 v2.2.4
 	github.com/rs/zerolog v1.34.0
+	github.com/stretchr/testify v1.11.1
 	github.com/urfave/cli/v3 v3.4.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ playback_test.go
@@ -0,0 +1,66 @@
+package main_test
+
+import (
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/jinzhu/copier"
+	"github.com/p-mng/goscrobble"
+	"github.com/stretchr/testify/require"
+)
+
+var defaultPlaybackStatus = main.PlaybackStatus{
+	Artists:   []string{"Placebo", "David Bowie"},
+	Track:     "Without You I'm Nothing",
+	Album:     "A Place For Us To Dream",
+	Duration:  time.Duration(time.Second * 251),
+	Timestamp: time.Unix(1699225080, 0),
+	State:     main.PlaybackPlaying,
+	Position:  time.Duration(time.Second * 110),
+}
+
+func TestPlaybackStatusJoinArtists(t *testing.T) {
+	require.Equal(t, "Placebo, David Bowie", defaultPlaybackStatus.JoinArtists())
+}
+
+func TestPlaybackStatusEquals(t *testing.T) {
+	copied := main.PlaybackStatus{}
+	err := copier.Copy(&copied, &defaultPlaybackStatus)
+	require.NoError(t, err)
+
+	require.True(t, defaultPlaybackStatus.Equals(copied))
+
+	copied.Album = "Meds"
+	require.False(t, defaultPlaybackStatus.Equals(copied))
+}
+
+func TestPlaybackStatusIsValid(t *testing.T) {
+	copied := main.PlaybackStatus{}
+	err := copier.Copy(&copied, &defaultPlaybackStatus)
+	require.NoError(t, err)
+
+	require.True(t, defaultPlaybackStatus.IsValid())
+
+	copied.Album = ""
+	require.False(t, copied.IsValid())
+}
+
+func TestPlaybackStatusRegexReplace(t *testing.T) {
+	copied := main.PlaybackStatus{}
+	err := copier.Copy(&copied, &defaultPlaybackStatus)
+	require.NoError(t, err)
+
+	require.Equal(t, "Without You I'm Nothing", defaultPlaybackStatus.Track)
+
+	copied.RegexReplace([]main.ParsedRegexReplace{
+		{
+			Match:   regexp.MustCompile("^Without You"),
+			Replace: "With You",
+			Artist:  false,
+			Track:   true,
+			Album:   false,
+		},
+	})
+	require.Equal(t, "With You I'm Nothing", copied.Track)
+}
